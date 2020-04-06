b977267a05caeaaec61f03daad65d156d8fe7fb8
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-04-06 08:46:31 -0400

    progress, I hope

--- src/templates/c_library_template/src/main.c
+++ src/templates/c_library_template/src/main.c
@@ -1,7 +1,10 @@
 #include <config.h>
 #include <stdio.h>
 
+#include "[% project.package_to_variable_name %].h"
+
 int main (void) {
 	puts ("Hello world from " PACKAGE_STRING);
+	[% project.package_to_variable_name %]_hello();
 	return 0;
 }
