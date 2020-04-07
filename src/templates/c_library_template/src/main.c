0c90e0a6bce176472c4d5b81f8e06f1023136ba6
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-04-06 21:53:33 -0400

    removing underscores from files probalby worked.

--- src/templates/c_library_template/src/main.c
+++ src/templates/c_library_template/src/main.c
@@ -1,7 +1,7 @@
 #include <config.h>
 #include <stdio.h>
 
-#include "[% project.package_to_variable_name %].h"
+#include "[% project.project_name %].h"
 
 int main (void) {
 	puts ("Hello world from " PACKAGE_STRING);
