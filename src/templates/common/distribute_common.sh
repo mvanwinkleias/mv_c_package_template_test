3ef9d333e57c0f1e3ed68488ffe06a08e1dd68ce
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-12-19 20:23:16 -0500

    common progress

--- src/templates/common/distribute_common.sh
+++ src/templates/common/distribute_common.sh
@@ -13,9 +13,14 @@ template_dirs=( \
 	"${script_dir}/../c_mpi_project/" \
 )
 
+# This (hopefully) will be temporary.  Instead of modifying the
+# globbing behavior with shopt -s dotglob, I just copy things
+# in two steps.
 for template_dir in "${template_dirs[@]}"
 do
     # echo "$template_dir is a template dir"
+
+	cp -r "$stuff_dir/".[^.]* "$template_dir/"
 	cp -r "$stuff_dir/"* "$template_dir/"
 done
 
