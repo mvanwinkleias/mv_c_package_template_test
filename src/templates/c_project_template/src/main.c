a31fe25429cb722af8d3557809ea74299c349334
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-01-02 20:25:19 -0500

    this might work.

--- src/templates/c_project_template/src/main.c
+++ src/templates/c_project_template/src/main.c
@@ -1,7 +1,36 @@
 #include <config.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int dump_a_text_file (void)
+{
+	FILE *file_pointer; 
+	char c;
+	char * file_name = DATADIR "/text/hello.txt";
+	puts ("File name: ");
+	puts(file_name);
+	file_pointer = fopen(file_name, "r"); 
+	if (file_pointer == NULL) 
+	{ 
+		printf("Cannot open file \n"); 
+		exit(1); 
+	} 
+  
+	c = fgetc(file_pointer); 
+	while (c != EOF) 
+	{ 
+		printf ("%c", c); 
+		c = fgetc(file_pointer); 
+	} 
+  
+	fclose(file_pointer); 
+	return 0; 
+}
 
 int main (void) {
 	puts ("Hello world from " PACKAGE_STRING);
+
+	dump_a_text_file();
+
 	return 0;
 }
