dbf606aec3c02c5dcfc1ca00366f28de85345780
Martin VanWinkle III <mvanwinkle@ias.edu>
2026-07-18 00:10:19 -0400

    unicde stuff

--- src/templates/c_library_template2/src/lib/[% project.project_name %].c
+++ src/templates/c_library_template2/src/lib/[% project.project_name %].c
@@ -1,7 +1,13 @@
 #include "[% project.project_name %].h"
 
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
 void [% project.package_to_variable_name %]_hello()
 {
-	printf("Hello, from [% project.package_to_variable_name %].\n");
+	setlocale(LC_CTYPE, "");
+	wchar_t knight = L'♘';
+	wprintf(L"Hello, from [% project.package_to_variable_name %]. %lc\n", knight);
 }
 
--- src/templates/c_library_template2/tests/test_[% project.project_name %].c
+++ src/templates/c_library_template2/tests/test_[% project.project_name %].c
@@ -1,11 +1,16 @@
 #include <config.h>
 #include <stdio.h>
 
+#include <wchar.h>
+#include <locale.h>
+
 #include "[% project.project_name %].h"
 
 int main(int argc, char *argv[])
 {
-	puts ("Hello world from " PACKAGE_STRING);
+	setlocale(LC_ALL, "");
+	wprintf(L"Hello world from %s\n", PACKAGE_STRING);
 	[% project.package_to_variable_name %]_hello();
+
 	return 0;
 }
